2d402c8db1ed8be8068ad4308202bff7bfc000ae
Olgaidukova <olya.gaidukova@gmail.com>
2023-01-01 14:04:26 +0300

    init commit

---
+++ src/test/java/GitHubConflict.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GitHubConflict {
+    @Test
+    void newTest() {
+        int i = 42;
+        System.out.println("This is the best test");
+        Assertions.assertTrue(i > 2);
+    }
+}
